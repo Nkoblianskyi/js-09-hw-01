2e1263b29eb62c1b5daf44e304c8c3b8191d1e4f
Nykyta <153436028+Nkoblianskyi@users.noreply.github.com>
2024-03-26 16:10:36 +0100

    homework1

--- js/01-color-switcher.js
+++ js/01-color-switcher.js
@@ -1,1 +1,27 @@
 
+const refs = {
+    bodyElements: document.querySelector('body'),
+    startBtn: document.querySelector('button[data-start]'),
+    stopBtn: document.querySelector('button[data-stop]'),
+
+};
+
+let timer = null;
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+    };
+
+const changeColorBack = () => { 
+    refs.bodyElements.style.background = getRandomHexColor();
+};
+
+refs.startBtn.addEventListener('click', () => {
+    timer = setInterval(changeColorBack, 1000);
+    refs.startBtn.disabled = true;
+});
+
+refs.stopBtn.addEventListener('click', () => {
+    clearInterval(timer);
+    refs.startBtn.disabled = false;
+});
